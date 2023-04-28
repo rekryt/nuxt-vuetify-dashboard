8df9b20006aabebc2706c809059cffab3f6db653
Rekryt <rekrytkw@gmail.com>
2023-04-28 21:13:14 +0300

    dependency updates

--- components/helper/Editor.vue
+++ components/helper/Editor.vue
@@ -1,3 +1,5 @@
 <template>
-    <ck-editor v-bind="$attrs" @input="$emit('update:modelValue', $event)"></ck-editor>
+    <lazy-client-only>
+        <ck-editor v-bind="$attrs" @input="$emit('update:modelValue', $event)"></ck-editor>
+    </lazy-client-only>
 </template>
